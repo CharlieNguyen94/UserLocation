3ee71e1c5df9e6fcc0c7dd1721246bb54f88b57d
CharlieNguyen94 <charlietugnuyen@hotmail.com>
2021-08-26 17:31:23 +0100

    Span edited and show user location is true

--- UserLocation/ContentView.swift
+++ UserLocation/ContentView.swift
@@ -11,10 +11,10 @@ import MapKit
 struct ContentView: View {
     
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054),
-                                                   span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+                                                   span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
     
     var body: some View {
-        Map(coordinateRegion: $region)
+        Map(coordinateRegion: $region, showsUserLocation: true)
             .ignoresSafeArea()
     }
 }
